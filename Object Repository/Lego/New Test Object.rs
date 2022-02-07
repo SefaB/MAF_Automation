3c1a7afb21b10c61608815432c6d846718bcec56
Sefa Bilgi <sefabilgi123@gmail.com>
2022-02-07 12:30:24 +0300

    folders sorted out

---
+++ Object Repository/Lego/New Test Object.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Test Object</name>
+   <tag></tag>
+   <elementGuidId>477a5c02-5323-4119-817e-f9e0130d7571</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
