c2fb1e4ca9707aa650fa7088351934d1da294b39
Vova Melnykov <v.melnikov@emakina.com.tr>
2022-02-11 11:02:19 +0200

    Fix conflicts

--- Scripts/CB2/Common/Browser open/Script1644411803178.groovy
+++ Scripts/CB2/Common/Browser open/Script1644411803178.groovy
@@ -19,5 +19,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
+WebUI.maximizeWindow()
+
 WebUI.navigateToUrl('https://cb2.c1xjddw2-majidalfu1-s1-public.model-t.cc.commerce.ondemand.com/en/')
 
