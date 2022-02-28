55225430ab25db187901d346147ef1a881726f11
TaniaFatkina <t.fatkina@raccoon.pet>
2022-02-28 13:48:12 +0200

    All Saints test cases

---
+++ Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_AED 250.00.rs
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_AED 250.00</name>
+   <tag></tag>
+   <elementGuidId>5de31109-ebea-4949-bf3d-331741b740d6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='undefined']/div/div/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.price</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>price</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>AED 250.00</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ProductListComponent&quot;)/div[@class=&quot;leo-product-list-component leo-product-list-component-product-grid-alpha-three-columns xs-up:product-grid:pos=rel xs-up:product-grid-col-item-inner:h=100 xs-up:ff=beta xs-up:hmin=(400px) md-up:hmin=(500px) lg-up:hmin=(700px) xs-up:product-grid-container:px=4 lg-up:product-grid-container:px=(5.5rem) lg-up:product-grid-container:wmax=100 xs-up:product-grid-row-group:mb=5 xs-up:product-grid-col-item-inner:d=flx xs-up:product-grid-col-item-inner:flx-dir=col xs-up:product-grid-col-item-inner:pos=rel xs-up:product-grid-col-item-inner:pr=0 md-up:product-grid-col-item-inner:pr=6 xs-up:product-grid-col-item-inner:pl=0 md-up:product-grid-col-item-inner:pl=0 lg-up:product-grid-col-item-inner:pl=0 xs-up.row-image+.row:mt=5 xs-up.row-image>.col-item>.col-item-inner:px=0! md-up.row-add-to-wishlist>>.col-item:hover>>.tile-add-to-wishlist:d=flx lg-up.tile-add-to-wishlist:d=none xs-up.tile-add-to-wishlist:pos-align=top-right xs-up.tile-add-to-wishlist>>.btn-wishlist:before:square=(1.333rem) xs-up.tile-add-to-wishlist:bottom=auto md-down>>.col-6>>.tile-add-to-wishlist:bottom=2 md-down>>.col-6>>.tile-add-to-wishlist:top=auto lg-up.tile-add-to-wishlist:top=n1 xs-up.tile-variant-selector-advanced>>.features-group:mb=3 xs-up.tile-variant-selected-item>>.variant-list:mb=3 xs-up.tile-name:pr=7 md-down>>.col-6>>.tile-name:pr=0 xs-up.tile-summary>>.summary:mb=3 xs-up.tile-add-to-cart:mt=auto xs-up.tile-image:d=flx xs-up.tile-image:h=100 xs-up.tile-image>div:w=100 xs-up:product-grid-alpha-two-columns lg-up:product-grid-alpha-three-columns md-down.tile-variant-selector-simple>>.size:d=none&quot;]/div[1]/div[@class=&quot;container container-grid&quot;]/div[@class=&quot;row-group&quot;]/div[@class=&quot;row row-add-to-wishlist row-badges row-name row-price row-variant-selector-simple row-add-to-cart row-stock-badge-cta&quot;]/div[@class=&quot;col-item col-4&quot;]/div[@class=&quot;col-item-inner&quot;]/div[@class=&quot;tile tile-price&quot;]/div[@id=&quot;undefined&quot;]/div[@class=&quot;leo-product-price-component leo-product-price-component-product-price-alpha xs-up&lt;&lt;.list-view>>.price:fs=(1.333rem) xs-up.price:fs=p2 md-up.price:fs=p1 xs-up.price:fw=normal xs-up.price.state-cross:fc=danger xs-up&lt;&lt;.container-grid>>.price:mb=1 xs-up&lt;&lt;.list-view>>.price:mb=0 xs-up.crossed-price:fc=black-70 xs-up&lt;&lt;.selected-combination-disabled:d=none xs-up.price:fw=semi product-price-alpha&quot;]/div[1]/div[@class=&quot;price&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='undefined']/div/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Make It Imogen Boy T-Shirt'])[1]/following::div[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Add to favorites'])[1]/following::div[13]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Selected option is no longer available.'])[1]/preceding::div[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='AED 250.00']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div[4]/div/div/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'AED 250.00' or . = 'AED 250.00')]</value>
+   </webElementXpaths>
+</WebElementEntity>
--- Scripts/Alls/Alls_Homepage/Verify PLP page Products/Script1645536141887.groovy
+++ Scripts/Alls/Alls_Homepage/Verify PLP page Products/Script1645536141887.groovy
@@ -17,3 +17,40 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://alls.c1xjddw2-majidalfu1-s1-public.model-t.cc.commerce.ondemand.com/en-ae')
+
+WebUI.click(findTestObject('Object Repository/Page_Official Website - Women  Men Fashion _e8583b/button_Accept Cookies'))
+
+WebUI.mouseOver(findTestObject('Object Repository/Page_Official Website - Women  Men Fashion _e8583b/span_Women'))
+
+WebUI.click(findTestObject('Object Repository/Page_Official Website - Women  Men Fashion _e8583b/a_T-Shirts'))
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_Womens T-ShirtsOur womens T-shirts are _c0c61d'), 
+    0)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/h1_Womens T-Shirts'), 
+    'Women\'s T-Shirts')
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/span_Womens T-Shirts'), 
+    'Women\'s T-Shirts')
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_Name (descending)_col-item col-4'))
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_AED 250.00'))
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/button_Add to Bag'))
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_M ML X XSS'))
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/span_46 OFF'))
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_On Sale'))
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_Sale AED 145.00'))
+
+WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Shop Womens T-Shirts Online  AllSaints UAE/div_reg. AED 270.00'))
+
+WebUI.closeBrowser()
+
