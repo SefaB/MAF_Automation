fe45ef2d9a75aa6fd752fd9593148602c0ca392e
Sefa Bilgi <sefabilgi123@gmail.com>
2022-03-07 09:43:11 +0300

    cab PLP

---
+++ Object Repository/CAB/CAB_PLP/PLP_First_Product_PriceTag.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>PLP_First_Product_PriceTag</name>
+   <tag></tag>
+   <elementGuidId>6c3f34ee-8ae1-493a-887c-c74cc696cc38</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;ProductListComponent&quot;]/div/div/div/div[1]/div[2]/div[1]/div/div[5]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
