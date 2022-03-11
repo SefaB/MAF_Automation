3248a9c57584062f4135abc298cf63a45d4fa3ce
Sefa Bilgi <sefabilgi123@gmail.com>
2022-03-11 14:24:45 +0300

    TEst Nadide

---
+++ Object Repository/CAB/CAB_PLP/Nadide.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Nadide</name>
+   <tag></tag>
+   <elementGuidId>a9cf9325-34a4-4682-9204-9335eccfa92c</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
