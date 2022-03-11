36882e334806c5625bab52196e4641a205a6605c
Sefa Bilgi <sefabilgi123@gmail.com>
2022-03-11 11:20:29 +0300

    deleting items

--- Object Repository/CAB/CAB_PLP/New Test Object.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>New Test Object</name>
-   <tag></tag>
-   <elementGuidId>45a66fc6-b2b6-4525-9805-c4b8a8d572b5</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
